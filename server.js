51f5fe1ec039761bf7d238697aeba6d63cace41f
Jonathan <Jonathangarces7@outlook.com>
2018-12-25 17:01:16 -0600

    correccion

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
 
     res.render('maintenance.hbs');
 
-});
+});*/
 
 /*app.get('/maintenance', (req, res) => {
 
